2850f88990fcf5d7b494c8848f63ca6ffd9a64d3
kumararpit135 <kumararpit994>
2025-07-23 22:01:58 +0530

    added new could cred

--- util/expense.js
+++ util/expense.js
@@ -2,6 +2,6 @@
 const Sequelize=require('sequelize')
 const exsequelize=new Sequelize('expense','root','20130008890',{ 
     dialect:'mysql',
-    host:'localhost'
+    host:'expense.cpi8aciwypm8.ap-south-1.rds.amazonaws.com'
 });
 module.exports=exsequelize;
